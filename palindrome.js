03495b734c8468298ce8b498bf5ca4668097df1a
Muhammad Talha Jan <122998881+talhajan-tj@users.noreply.github.com>
2023-11-22 14:49:33 +0500

    Palindrome final

--- palindrome.js
+++ palindrome.js
@@ -1,7 +1,34 @@
-function palindrome(str) {
-  const formateStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
-  const reversedStr = formateStr.split("").reverse().join("");
-  return formateStr === reversedStr;
-}
+// function palindrome(str) {
+//   const formateStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+//   const reversedStr = formateStr.split("").reverse().join("");
+//   return formateStr === reversedStr;
+// }
 
-console.log(palindrome("maDam"));
+function isPalindrome(str) {
+  const formatedString = removeNonAlphaNumeric(str.toLowerCase());
+  const reversedString = reverseString(formatedString);
+  return formatedString === reversedString;
+}
+function removeNonAlphaNumeric(str) {
+  let formatedString = "";
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (isAlphaNumeric(char)) {
+      formatedString += char;
+    }
+  }
+  return formatedString;
+}
+function isAlphaNumeric(char) {
+  const code = char.charCodeAt(0);
+
+  return (code >= 48 && code <= 57) || (code >= 97 && code <= 122);
+}
+function reverseString(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+console.log(isPalindrome("maDa@m"));
