fa527cdb2f469a7f373bd9f5ca53bed566f2fa19
Muhammad Talha Jan <122998881+talhajan-tj@users.noreply.github.com>
2023-12-22 13:58:07 +0500

    Array Intersection

--- array-intersection.js
+++ array-intersection.js
@@ -1,13 +1,28 @@
-function arrayIntersection(arr1, arr2) {
-  let newArray = [];
-  for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      if (arr1[i] === arr2[j]) {
-        newArray.push(arr2[j]);
-      }
-    }
-  }
-  return newArray;
-}
+//By TJ
 
+// function arrayIntersection(arr1, arr2) {
+//   let newArray = [];
+//   for (let i = 0; i < arr1.length; i++) {
+//     for (let j = 0; j < arr2.length; j++) {
+//       if (arr1[i] === arr2[j]) {
+//         newArray.push(arr2[j]);
+//       }
+//     }
+//   }
+//   return newArray;
+// }
+
+// console.log(arrayIntersection([1, 3, 5, "abc"], ["abc", 2, 3, 7, 11, 1]));
+
+// Another mEthod -> Brad
+
+function arrayIntersection(arr1, arr2) {
+  const set1 = new Set(arr1);
+  let intersectArray = [];
+  for (let num of arr2)
+    if (set1.has(num)) {
+      intersectArray.push(num);
+    }
+  return intersectArray;
+}
 console.log(arrayIntersection([1, 3, 5, "abc"], ["abc", 2, 3, 7, 11, 1]));
