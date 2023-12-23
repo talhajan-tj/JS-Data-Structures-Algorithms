cbefb603575d668b2f92435dc83b8b0c26dd9346
Muhammad Talha Jan <122998881+talhajan-tj@users.noreply.github.com>
2023-12-23 17:59:41 +0500

    Unique Chars

---
+++ unique-chars.js
@@ -0,0 +1,13 @@
+function uniqueChars(str) {
+  const charCount = {};
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charCount[char]) {
+      return false;
+    }
+    charCount[char] = true;
+  }
+  return true;
+}
+console.log(uniqueChars("stringr"));
+console.log(uniqueChars("talha"));
